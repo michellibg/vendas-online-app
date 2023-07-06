25225ed57e40d9f8195d92808280cfc60b9c790b
Michelli Moya <michellibg@gmail.com>
2023-07-06 17:55:58 -0300

    styles components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView,StyleSheet, Text, View, Button} from 'react-native';
+
+import styled from 'styled-components/native';
+
+
+const TextNew = styled.Text`
+    font-size: 24px;
+    color: blue;
+    
+`;
+  
 
 const App = () => {
   return (
     <View>
       <SafeAreaView>
-        <Text>Testando</Text>
+        <Text style={styles.container}>Testando</Text>
+        <TextNew>Titulo teste</TextNew>
       </SafeAreaView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+})
 
 export default App;
