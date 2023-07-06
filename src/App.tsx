52f1d199cb929a250c7cf4d93640c068e4753921
Michelli Moya <michellibg@gmail.com>
2023-07-06 14:58:42 -0300

    start project react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const App = () => {
+
+    return <View>
+        <SafeAreaView>
+        <Text>Testando</Text>
+        </SafeAreaView>        
+    </View>
+}
+
+export default App;
