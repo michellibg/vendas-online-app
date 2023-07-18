e866d107e1052deadf64da38e53110b717f8ff60
Michelli Moya <michellibg@gmail.com>
2023-07-18 17:39:47 -0300

    component button

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,14 +1,21 @@
 import { Text, TextInput, View } from "react-native";
 import { ContainerLogin } from "../styles/login.style";
 import Input from "../../../shared/components/input/input";
+import Button from "../../../shared/components/button/Button";
 
 const Login = () => {
+
+
+    const handleOnPress = () => {
+        console.log('clicou');
+    }
 
 return (
     <View>
         <ContainerLogin>
             <Text>Login</Text>
             <Input/>
+            <Button margin ="16px" title='ENTRAR' onPress={handleOnPress}></Button>
         </ContainerLogin>
         
     </View>
